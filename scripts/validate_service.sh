444ffcd1ab8cb84a791761a8d8848f8f55f84c1d
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 18:37:51 -0300

    Update appspec to aws ec2 deploy

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -3,6 +3,11 @@ if [ docker container inspect -f '{{.State.Running}}' "$(docker ps --filter name
   docker container prune -f
   docker image prune -af
 else
-  docker run -d -p 8080:80 pauloeduardods-front-end-old
+  docker rm -f pauloeduardods-front-end-1
+  docker rmi -f pauloeduardods-front-end
+  docker image tag pauloeduardods-front-end_old pauloeduardods-front-end
+  docker rmi -f pauloeduardods-front-end_old
+  docker container rename pauloeduardods-front-end-old pauloeduardods-front-end-1
+  docker run -d -p 8080:80 pauloeduardods-front-end-1
   exit 0
 fi
