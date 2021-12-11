bd6d5b247fd13c914d45a1a411ae7851f400fe5b
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 20:22:50 -0300

    remove container and image

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -4,4 +4,5 @@ echo "Stopping application..."
 
 docker container stop "$(docker ps --filter name=pauloeduardods-front-end-* -aq)"
 
+docker rm -f pauloeduardods-front-end-1
 docker image rmi -f pauloeduardods_front-end:latest
