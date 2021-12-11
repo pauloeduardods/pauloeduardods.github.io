f6202a2855c58f65ee0ef155f3e6af53d444c9ce
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 18:49:51 -0300

    Update validateService

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,5 +1,5 @@
 
-if [ docker container inspect -f '{{.State.Running}}' "$(docker ps --filter name=name=pauloeduardods-front-end-* -nq)" == "true" ]; then
+if [ docker container inspect -f '{{.State.Running}}' "$(docker ps --filter name=name=pauloeduardods-front-end-1 -nq)" == "true" ]; then
   docker container prune -f
   docker image prune -af
 else
