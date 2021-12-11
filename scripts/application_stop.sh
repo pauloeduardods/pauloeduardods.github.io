73359cc7e1ed96ff251dfdc6c2ce9c48f564a3ef
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 18:57:10 -0300

    Update validateService and application_stop

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -6,6 +6,6 @@ docker container stop "$(docker ps --filter name=pauloeduardods-front-end-* -aq)
 
 docker container rename pauloeduardods-front-end-1 pauloeduardods-front-end-old
 
-docker image tag pauloeduardods-front-end pauloeduardods-front-end_old
+docker image tag pauloeduardods-front-end:latest pauloeduardods-front-end_old:latest
 
-docker image rmi -f pauloeduardods-front-end
+docker image rmi -f pauloeduardods-front-end:latest
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -6,7 +6,7 @@ else
   docker rm -f pauloeduardods-front-end-1
   docker rmi -f pauloeduardods-front-end
   docker image tag pauloeduardods-front-end_old pauloeduardods-front-end
-  docker rmi -f pauloeduardods-front-end_old
+  docker rmi -f pauloeduardods-front-end_old:latest
   docker container rename pauloeduardods-front-end-old pauloeduardods-front-end-1
   docker run -d -p 8080:80 pauloeduardods-front-end-1
   exit 0
