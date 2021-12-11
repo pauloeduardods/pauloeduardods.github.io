b5cd44931f13cdb7488fc484ea0f81ecae052b96
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 19:54:18 -0300

    Remove app validation and add check if app is runnig on application_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+echo "Starting application"
+
 cd /home/ec2-user/pauloeduardods
 
 docker-compose up -d
+
+sleep 10
+
+echo "Checking if application is running"
+
+if [ "$( docker container inspect -f '{{.State.Status}}' pauloeduardods-front-end-1 )" == "running" ];
+then
+  echo "Container is running"
+  docker container prune -f
+  docker image prune -af
+  exit 0
+else 
+  echo "Container is not running"
+    docker rm -f pauloeduardods-front-end-1
+    docker rmi -f pauloeduardods_front-end:latest
+    docker image tag pauloeduardods_front-end_old:latest pauloeduardods_front-end:latest
+    docker rmi -f pauloeduardods_front-end_old:latest
+    docker container rename pauloeduardods-front-end-old pauloeduardods-front-end-1
+    docker run -d -p 8080:80 --name pauloeduardods-front-end-1 pauloeduardods_front-end-1:latest
+    docker container prune -f
+    docker image prune -af
+    exit 1
+fi
