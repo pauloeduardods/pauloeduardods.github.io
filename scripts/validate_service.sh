5d8abbbe3f8c1d09a5f74e75382933d7d2969d85
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 19:40:50 -0300

    Update validate_service

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,12 +1,16 @@
-# if [ docker container inspect -f '{{.State.Running}}' "$(docker ps --filter name=name=pauloeduardods-front-end-1 -q)" == "true" ]; then
-#   docker container prune -f
-#   docker image prune -af
-# else
-#   docker rm -f pauloeduardods-front-end-1
-#   docker rmi -f pauloeduardods_front-end:latest
-#   docker image tag pauloeduardods_front-end_old:latest pauloeduardods_front-end:latest
-#   docker rmi -f pauloeduardods_front-end_old:latest
-#   docker container rename pauloeduardods-front-end-old pauloeduardods-front-end-1
-#   docker run -d -p 8080:80 pauloeduardods-front-end-1
-#   exit 0
-# fi
+if [ "$( docker container inspect -f '{{.State.Status}}' pauloeduardods-front-end-1 )" == "running" ];
+then
+  echo "Container is running"
+  docker container prune -f
+  docker image prune -af
+  exit 0
+else 
+  echo "Container is not running"
+    docker rm -f pauloeduardods-front-end-1
+    docker rmi -f pauloeduardods_front-end:latest
+    docker image tag pauloeduardods_front-end_old:latest pauloeduardods_front-end:latest
+    docker rmi -f pauloeduardods_front-end_old:latest
+    docker container rename pauloeduardods-front-end-old pauloeduardods-front-end-1
+    docker run -d -p 8080:80 pauloeduardods-front-end-1
+    exit 1
+fi
