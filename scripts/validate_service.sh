d34ac9c713ff2b0b7d285b92816fc7d4c3076538
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 19:00:39 -0300

    Update validateService

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -4,8 +4,8 @@ if [ docker container inspect -f '{{.State.Running}}' "$(docker ps --filter name
   docker image prune -af
 else
   docker rm -f pauloeduardods-front-end-1
-  docker rmi -f pauloeduardods-front-end
-  docker image tag pauloeduardods-front-end_old pauloeduardods-front-end
+  docker rmi -f pauloeduardods-front-end:latest
+  docker image tag pauloeduardods-front-end_old:latest pauloeduardods-front-end:latest
   docker rmi -f pauloeduardods-front-end_old:latest
   docker container rename pauloeduardods-front-end-old pauloeduardods-front-end-1
   docker run -d -p 8080:80 pauloeduardods-front-end-1
