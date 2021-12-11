26f1520524d5f546a6ed5c0e5ef970c794e4f976
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 20:18:56 -0300

    Remove rollback when app is not runnig

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -4,8 +4,4 @@ echo "Stopping application..."
 
 docker container stop "$(docker ps --filter name=pauloeduardods-front-end-* -aq)"
 
-docker container rename pauloeduardods-front-end-1 pauloeduardods-front-end-old
-
-docker image tag pauloeduardods_front-end:latest pauloeduardods_front-end_old:latest
-
 docker image rmi -f pauloeduardods_front-end:latest
