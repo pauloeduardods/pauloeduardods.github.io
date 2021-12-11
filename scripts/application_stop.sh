4dee490c0c4b23f152f5e9c40ba627aa08c4f503
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-12-11 20:27:26 -0300

    Stop removing old images on application_stop

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,6 +3,3 @@
 echo "Stopping application..."
 
 docker container stop "$(docker ps --filter name=pauloeduardods-front-end-* -aq)"
-
-docker rm -f pauloeduardods-front-end-1
-docker image rmi -f pauloeduardods_front-end:latest
