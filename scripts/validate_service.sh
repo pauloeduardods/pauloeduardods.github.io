828abdb91686fe18ef6ec58e405a908e8278b9fd
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-12-13 11:31:27 -0300

    Add script to restart old version if deploy fails

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -8,7 +8,10 @@ then
   exit 0
 else 
   echo "Container is not running"
-    docker container prune -f
-    docker image prune -af
-    exit 1
+  echo "Restarting old version"
+  cd $(ls | head -1)
+  docker-compose up -d
+  docker container prune -f
+  docker image prune -af
+  exit 1
 fi
