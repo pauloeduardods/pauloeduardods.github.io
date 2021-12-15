991c70ec812f0f60ca13f5dcf39aaa2ece20e041
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-12-15 14:12:47 -0300

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ReactGa from 'react-ga';
 import Home from './pages/Home';
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize(process.env.GOOGLE_ANALYTICS_ID);
+    ReactGa.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Switch>
       <Route exact path="/" component={ Home } />
