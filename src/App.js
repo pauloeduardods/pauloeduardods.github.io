8aa562baf75c50475e1b723bb6fa90a74e035f5d
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-12-15 15:15:19 -0300

    Test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './pages/Home';
 
 function App() {
   useEffect(() => {
+    console.log(process.env.NODE_ENV)
+    console.log(process.env.GOOGLE_ANALYTICS_ID)
     if (process.env.NODE_ENV === 'production') {
       ReactGa.initialize(process.env.GOOGLE_ANALYTICS_ID);
       ReactGa.pageview(window.location.pathname + window.location.search);
