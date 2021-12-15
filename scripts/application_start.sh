1f1e376ad7320d2d0c78b96f8117cd0b1c78d109
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2021-12-15 19:47:40 -0300

    Test CD environment

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,6 @@ echo "Starting application"
 
 cd /home/ec2-user/pauloeduardods
 
+echo $GOOGLE_ANALYTICS_ID
+
 docker-compose up -d
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -5,3 +5,5 @@ echo "Stopping application..."
 docker container stop "$(docker ps --filter name=pauloeduardods-front-end-* -aq)"
 docker container prune -f
 docker image prune -af
+
+echo $GOOGLE_ANALYTICS_ID
