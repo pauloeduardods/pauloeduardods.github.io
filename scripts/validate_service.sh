1994b22d5bc96b2ced57bf2662078fab2dff9f86
Paulo Eduardo DSG <pauloeduardodesordigomes@gmail.com>
2022-02-08 18:01:15 -0300

    Update appSpec

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,17 +1,15 @@
+echo "Removing unused containers and images"
+
+docker container prune -f
+docker image prune -af
+
 echo "Checking if application is running"
 
 if [ "$( docker container inspect -f '{{.State.Status}}' pauloeduardods-front-end-1 )" == "running" ];
 then
   echo "Container is running"
-  docker container prune -f
-  docker image prune -af
   exit 0
 else 
   echo "Container is not running"
-  echo "Restarting old version"
-  cd $(ls | head -1)
-  docker-compose up -d
-  docker container prune -f
-  docker image prune -af
   exit 1
 fi
